faa6220ebd7d32af67dfe1860e1edbab6126ab6a
Osysh <max.abylon@gmail.com>
2021-04-22 20:08:07 +0200

    Page Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { CardBar } from './components/card_bar';
+
+import { HowTo } from './components/howto';
+import { About } from './components/about';
+import { Search } from './components/search';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="Header"><Header /></div>
+        <div className="Main-container">
+        <Switch>
+          <Route path='/' exact component={Search} />
+          <Route path='/howto' component={HowTo} />
+          <Route path='/about' component={About} />
+            </Switch>
+        </div>
+        <div className="Footer">
+            <Footer />
+          </div>
+        <div className="Card-bar"><CardBar /></div>
+      </div>
+    </Router>
   );
 }
 
